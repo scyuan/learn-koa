436b00fe9856faa39bedd14c2350679955f0db0c
scyuan <644397334@qq.com>
2018-09-21 19:23:35 +0800

    从自定义的request对象取值

--- request.js
+++ request.js
@@ -1,5 +1,14 @@
 let url = require('url');
 let request ={
+    // get 用法为对象的访问器类似于下面
+    // Object.defineProperty('name',{
+    //    set:function(value){
+    //          this.name = value;
+    //    },
+    //    get:function(){
+    //        return this.name;
+    //    }
+    // })
     get url(){
         //这样可通过ctx.request.url 直接取值了。不需要再通过req
         return this.req.url;
